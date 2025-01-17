60262485cf9d8e0908f1e743033c87a8d52e7388
aditya-12 <adityakaushik2011@gmail.com>
2025-01-16 18:12:32 -0600

    1. created signup API(generated token and cookies also)

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,9 +1,36 @@
+import { redis } from "../lib/redis.js";
 import User from "../models/user.model.js";
-// export const login = async (req,res)=> {
+import jwt from "jsonwebtoken";
 
+const generateToken = async (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
 
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+  return { accessToken, refreshToken };
+};
 
-// };
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(`refreshToken:${userId}`, refreshToken, "EX", "7*24*60*60");
+};
+
+const setCookies = async (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    maxAge: 15 * 60 * 1000,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+  });
+};
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -13,12 +40,21 @@ export const signup = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
     const user = await User.create({ email, password, name });
+
+    const { accessToken, refreshToken } = generateToken(User._id);
+    await storeRefreshToken(user._id, refreshToken);
+    setCookies(res, accessToken, refreshToken);
+
     res.status(201).json({ user, message: "user created successfully" });
   } catch (error) {
     console.log("Error in signup controller", error.message);
     res.status(500).json({ message: error.message });
   }
 };
+
+// export const login = async (req,res)=> {
+
+// };
 
 // export const logout = async (req, res) => {
 
